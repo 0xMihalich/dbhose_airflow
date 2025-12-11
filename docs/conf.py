b2d745ba54be88bdd170a5f6fdeb53b1850e2c27
Polupoltinnyikh.MM <polupoltinnyikh.mm@dns-shop.ru>
2025-12-11 16:28:59 +1000

    Docs: change conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,31 +1,21 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'DBHose-Airflow'
-copyright = '2025, 0xMihalich'
+copyright = '%Y, 0xMihalich'
 author = '0xMihalich'
 release = '0.1.0.1'
-
+master_doc = "index"
 language = 'ru'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'alabaster'
 html_static_path = ['_static']
 html_show_sphinx = False
+html_show_source = False
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_copybutton",
+]
+autodoc_type_aliases = {
+}
+autodoc_typehints = "description"
+templates_path = ["_templates"]
+exclude_patterns = []
+html_theme = "furo"
+html_static_path = ["_static"]
